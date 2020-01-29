e7fa6d0209dce4738ae3cee85726056418d04ed6
Yeison <yrrodriguezb@gmail.com>
2020-01-28 22:34:40 -0500

    Se configura el proyecto para permitir especificar el tamaño limite de los archivos assets

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,15 @@
 // const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
 
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/app-music-deezer/'
+    : '/',
+  chainWebpack: config => {
+    config.performance
+      .maxEntrypointSize(1000000)
+      .maxAssetSize(1000000)
+  }
   // configureWebpack: {
   //   plugins: [
   //     new VuetifyLoaderPlugin()
